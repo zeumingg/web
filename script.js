7edfb4fa18b733ca4b442b6a9c62aca7439a342f
zeumingg <zeumingcontacto@gmail.com>
2025-03-06 19:39:51 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const envelope = document.getElementById('envelope');
-const button = document.getElementById('toggleButton');
-
-button.addEventListener('click', () => {
-    envelope.classList.toggle('open');
-});
